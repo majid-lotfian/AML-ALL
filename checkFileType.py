8c1a25316f853b5274e8906553fe82af2057b9e7
Majid Lotfian Delouee <74425635+majid-lotfian@users.noreply.github.com>
2026-04-17 11:09:27 +0200

    Change Excel file reading engine to 'pyxlsb'

--- checkFileType.py
+++ checkFileType.py
@@ -3,8 +3,7 @@ import pandas as pd
 file_path = r"I:\AML_ALL\Data\BC_labuitslag_13-04-2026.csv"
 
 try:
-    df = pd.read_excel(file_path, nrows=10)
-    print("SUCCESS: It is actually an Excel file")
+    df = pd.read_excel(file_path, engine="pyxlsb", nrows=10)
     print(df.head())
 except Exception as e:
     print("FAILED:", e)
